11abedf7b70897bb4443efa0c5aada7031fa8f29
Kajal-Rajput-05 <161593122+Kajal-Rajput-05@users.noreply.github.com>
2024-04-20 14:26:56 +0530

    Add files via upload

---
+++ Coding_Challenge(20th April)/CQ8.java
@@ -0,0 +1,18 @@
+
+// bydefault whenever we use add function it add at last position of list
+import java.util.*;
+class CQ8 {
+  public static void main(String[] args) {
+	  List<String> list = new ArrayList<String>();
+	  
+	  list.add("Red");
+	  list.add("Orange");
+	  list.add("Yellow");
+	  list.add("Green");
+	  list.add("Blue");
+	  list.add("Indigo");
+	  list.add("Violet");
+	  
+	  System.out.println(list);
+	 }
+}
