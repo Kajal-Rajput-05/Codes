1c588bd1d33e072314f9901e31a477032f03940d
Kajal-Rajput-05 <161593122+Kajal-Rajput-05@users.noreply.github.com>
2024-04-20 14:19:04 +0530

    Add files via upload.

--- Coding_Challenge(20th April)/CQ10.java
+++ Coding_Challenge(20th April)/CQ10.java
@@ -1,97 +1,16 @@
-/*
-class CQ10{
-		Node head;
-		
-		static class Node{
-			int data;
-			Node link;
-		
-			Node(int d){
-				data = d;
-				link = null;
-		}
-	}
 
-		public void insertEnd( int new_data ){
-			Node new_node = new Node(new_data);
-			if(head == null){
-				head = new Node(new_data);
-				return;
-			}
-			new_node.link = null;
-			Node last = head;
-			while( last.link != null )
-				last = last.link;
-				last.link = new_node;
-				return;
-		}
-		
-	    public void display(){
-		Node n = head;
-		while( n != null ){
-		    System.out.print(n.data + " -> "); 
-            n = n.link; 
-		}			
-	}
-	
-	public static void main(String args[]){
-		
-		CQ10 l1 = new CQ10();
-		l1.head = new Node(11);	//head connected to first node
-		Node second = new Node(22);	// node created 
-		Node third = new Node(33);	// node created
-		
-		l1.head.link = second;	//link connection
-		second.link = third;	// link connection
-	
-		l1.insertEnd( 44 );
-		l1.insertEnd( 55 );
-		l1.display();
-	}
-}
-*/
-
-//------------------------------------------------------
-
-//insert at first position
-
- class CQ10{
-		Node head;
-		
-		static class Node{
-			int data;
-			Node link;
-		
-			Node(int d){
-				data = d;
-				link = null;
-		}
-	}
-		public void insert(  ){
-			Node new_node = new Node(new_data);
-			new_node.link = head;
-			head = new node;
-		}
-		
-	    public void display(){
-		Node n = head;
-		while( n != null ){
-		    System.out.print(n.data + " -> "); 
-            n = n.link; 
-		}			
-	}
-	
-	public static void main(String args[]){
-		
-		CQ10 l1 = new CQ10();
-		l1.head = new Node(11);	//head connected to first node
-		Node second = new Node(22);	// node created 
-		Node third = new Node(33);	// node created
-		
-		l1.head.link = second;	//link connection
-		second.link = third;	// link connection
-				
-		l1.insert(10);
-		l1.display();
-	}
+import java.util.*; 
+ class CQ10 { 
+ 
+    public static void main(String args[]) { 
+        LinkedList<String> list = new LinkedList<String>(); 
+  
+        list.add("Kajal"); 
+        list.add("Rajput"); 
+  
+        list.addFirst("047"); 
+        list.addLast("KH"); 
+ 
+        System.out.println(list); 
+    } 
 }
